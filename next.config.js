86c61e3078d6ad1ba1f5570c17dbdd4fbbcf482b
stevensoneremeh <techcitybystevenson@zohomail.com>
2025-08-29 01:50:55 +0100

    Complete ABL NATASHA ENTERPRISES Production Platform

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
